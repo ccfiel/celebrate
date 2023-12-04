b79fe3042880dca41e284a8b4879f1aeba08c07a
Chris Ian Fiel <ccfiel@gmail.com>
2023-12-04 06:13:45 +0000

    Fix bug in login functionality

---
+++ celebrate/celebrate/doctype/celebrate_settings/celebrate_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, KaHero Apps Inc and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Celebrate Settings", {
+// 	refresh(frm) {
+
+// 	},
+// });
