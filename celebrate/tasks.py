a89aa0b9476f2d8faee209d27ea65425117b766f
Chris Ian Fiel <ccfiel@gmail.com>
2023-12-04 07:41:52 +0000

    fix bug

--- celebrate/tasks.py
+++ celebrate/tasks.py
@@ -58,14 +58,18 @@ def greet_message(type: str):
     date_today  = frappe.utils.data.getdate()
     day = date_today.strftime("%d")
     month = date_today.strftime("%m")
+    year_now = date_today.strftime("%Y")
+    
     employees = frappe.db.sql("""select first_name, last_name, {0} from tabEmployee where status='Active' and DAY({0})={1} and MONTH({0})={2}""".format(field_date, day, month))
     for employee in employees:
-        first_name = employee[0]
-        last_name = employee[1]
-        celebrate_date = employee[2]
-        employee_name = first_name.strip() + " " + last_name.strip()
-        years = pretty_date(celebrate_date.strftime('%Y-%m-%d %H:%M:%S.%f')).replace(' ago', '')        
+        year_celebrate = celebrate_date.strftime('%Y')
+        if year_now != year_celebrate:
+            first_name = employee[0]
+            last_name = employee[1]
+            celebrate_date = employee[2]
+            employee_name = first_name.strip() + " " + last_name.strip()
+            years = pretty_date(celebrate_date.strftime('%Y-%m-%d %H:%M:%S.%f')).replace(' ago', '')        
 
-        pick_message = random.randint(0, len(messages) - 1)
-        greeting_message = messages[pick_message].strip().replace('{name}', employee_name).replace('{years}', years)
-        notify_chat(title + ', ' + employee_name, color, greeting_message, [])
+            pick_message = random.randint(0, len(messages) - 1)
+            greeting_message = messages[pick_message].strip().replace('{name}', employee_name).replace('{years}', years)
+            notify_chat(title + ', ' + employee_name, color, greeting_message, [])
